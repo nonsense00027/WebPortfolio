273f7c91adae6044995f368eca5c1aebc426ebaa
nonsense00027 <caneteneilfred@gmail.com>
2020-06-03 00:41:57 +0800

    Skills added and modified javascript file

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,43 @@
-//ACTIVE BUTTON
-const menuButtons = document.querySelectorAll(".btn-menu");
-
+// FUNCTIONS
 function toggleActive(e) {
   removeActive();
+  removeActiveCategory();
   this.classList.add("active");
+  skillItem.forEach((skill) => (skill.style.width = "0"));
 }
 
 function removeActive() {
   menuButtons.forEach((button) => button.classList.remove("active"));
 }
+
+function toggleMenu() {
+  menuButton.classList.toggle("close");
+  menu.classList.toggle("show");
+  menuNav.classList.toggle("show");
+  navItems.forEach((item) => item.classList.toggle("show"));
+}
+
+function toggleActiveCategory(e) {
+  console.log(e.target.id);
+  animate(e.target.id);
+  removeActiveCategory();
+  this.classList.add("active");
+}
+
+function animate(id) {
+  if (id === "prog") {
+    progSkill.forEach((prog) => (prog.style.width = "100px"));
+  }
+}
+
+function removeActiveCategory() {
+  categoryButton.forEach((button) => button.classList.remove("active"));
+}
+
+//ACTIVE BUTTON
+const menuButtons = document.querySelectorAll(".btn-menu");
+const skillItem = document.querySelectorAll("#skill-item");
+
 menuButtons.forEach((button) => button.addEventListener("click", toggleActive));
 
 //MENU BUTTON
@@ -21,24 +50,10 @@ let showMenu = false;
 
 menuButton.addEventListener("click", toggleMenu);
 
-function toggleMenu() {
-  menuButton.classList.toggle("close");
-  menu.classList.toggle("show");
-  menuNav.classList.toggle("show");
-  navItems.forEach((item) => item.classList.toggle("show"));
-}
-
 // CATEGORY BUTTON
-const categoryButton = document.querySelectorAll(".btn");
+const categoryButton = document.querySelectorAll(".category-item");
+const progSkill = document.querySelectorAll(".prog");
 
-function toggleActiveCategory(e) {
-  removeActiveCategory();
-  this.classList.add("active");
-}
-
-function removeActiveCategory() {
-  categoryButton.forEach((button) => button.classList.remove("active"));
-}
 categoryButton.forEach((button) =>
   button.addEventListener("click", toggleActiveCategory)
 );
