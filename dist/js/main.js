9cb2a5e29ece36bb8d99d682151edbc18d728a48
nonsense00027 <caneteneilfred@gmail.com>
2020-06-08 21:40:04 +0800

    Implemented Mobile First

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,58 +1,140 @@
-// FUNCTIONS
-function toggleActive(e) {
-  removeActive();
-  removeActiveCategory();
-  this.classList.add("active");
-  skillItem.forEach((skill) => (skill.style.width = "0"));
+// MENU BUTTON FUNCTIONS
+function toggleMenu() {
+  whole.classList.toggle("active");
+  menuButton.classList.toggle("close");
+  menu.classList.toggle("show");
+  header.classList.toggle("active");
 }
 
+// MENU NAV FUNCTIONS
 function removeActive() {
   menuButtons.forEach((button) => button.classList.remove("active"));
 }
 
-function toggleMenu() {
-  menuButton.classList.toggle("close");
-  menu.classList.toggle("show");
-  menuNav.classList.toggle("show");
-  navItems.forEach((item) => item.classList.toggle("show"));
-}
-
-function toggleActiveCategory(e) {
-  console.log(e.target.id);
-  animate(e.target.id);
+function toggleActive(e) {
+  toggleMenu();
+  removeActive();
   removeActiveCategory();
   this.classList.add("active");
+  resetSkillItem();
 }
 
-function animate(id) {
-  if (id === "prog") {
-    progSkill.forEach((prog) => (prog.style.width = "100px"));
-  }
+// SKILLS FUNCTIONS
+function toogleBadges(type) {
+  badges.forEach((badge) => {
+    if (type === 0) {
+      badge.classList.add("active");
+    } else {
+      badge.classList.remove("active");
+    }
+  });
 }
-
 function removeActiveCategory() {
   categoryButton.forEach((button) => button.classList.remove("active"));
 }
 
-//ACTIVE BUTTON
-const menuButtons = document.querySelectorAll(".btn-menu");
-const skillItem = document.querySelectorAll("#skill-item");
+function removeAnimation() {
+  document.querySelector(".animation").classList.remove("show");
+  setTimeout(() => {
+    document.querySelector(".circle").style.animationName = "";
+  }, 1000);
+}
 
+function resetSkillItem() {
+  skillItem.forEach((skill) => (skill.style.width = "0"));
+}
+
+function animate(id) {
+  if (id === "prog") {
+    skillItem.forEach((skill) => {
+      const skillId = skill.id.split(" ");
+      if (skillId[0] === "prog") {
+        animation(skill, skillId[1]);
+      }
+    });
+  } else if (id === "design") {
+    skillItem.forEach((skill) => {
+      const skillId = skill.id.split(" ");
+      if (skillId[0] === "design") {
+        animation(skill, skillId[1]);
+      }
+    });
+  } else if (id === "web") {
+    skillItem.forEach((skill) => {
+      const skillId = skill.id.split(" ");
+      if (skillId[0] === "web") {
+        animation(skill, skillId[1]);
+      }
+    });
+  }
+}
+
+function animation(skill, type) {
+  // console.log(skill);
+  if (type === "in") {
+    skill.style.width = "35vw";
+  } else if (type === "ad") {
+    skill.style.width = "45vw";
+  } else if (type === "ex") {
+    skill.style.width = "60vw";
+  } else skill.style.width = "25vw";
+}
+
+function toggleActiveCategory(e) {
+  removeActiveCategory();
+  // showAnimation();
+  document.querySelector(".circle").style.animationName = "loading";
+  document.querySelector(".animation").classList.add("show");
+  setTimeout(() => {
+    removeAnimation();
+    resetSkillItem();
+    animate(e.target.id);
+    this.classList.add("active");
+  }, 3000);
+}
+
+//SECTION OBSERVER FUNCTIONS
+const options = {
+  // rootMargin: "-1000px 0px 0px 0px",
+  threshold: 0.35,
+};
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry.target);
+      if (entry.target.id === "skills") {
+        toogleBadges(0);
+      } else {
+        toogleBadges(1);
+      }
+    }
+  });
+}, options);
+
+// SECTIONS OBSERVER
+const sections = document.querySelectorAll("section");
+console.log(sections);
+sections.forEach((section) => {
+  observer.observe(section);
+});
+const whole = document.querySelector(".whole");
+const menuButton = document.querySelector(".menu-btn");
+const header = document.querySelector(".header");
+menuButton.addEventListener("click", toggleMenu);
+const menu = document.querySelector(".menu");
+const title = document.querySelector(".title");
+const about = document.querySelector(".about-me");
+const content = document.querySelector(".content");
+const subContent = document.querySelector(".sub-content");
+
+// ACTIVE BUTTON
+const menuButtons = document.querySelectorAll(".btn-menu");
 menuButtons.forEach((button) => button.addEventListener("click", toggleActive));
 
-//MENU BUTTON
-const menuButton = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const navItems = document.querySelectorAll(".nav-item");
-
-let showMenu = false;
-
-menuButton.addEventListener("click", toggleMenu);
-
-// CATEGORY BUTTON
+// SKILLS CATEGORY
 const categoryButton = document.querySelectorAll(".category-item");
-const progSkill = document.querySelectorAll(".prog");
+const skillItem = document.querySelectorAll(".skill-item");
+const badges = document.querySelectorAll(".badge");
 
 categoryButton.forEach((button) =>
   button.addEventListener("click", toggleActiveCategory)
