cb8471394ba6a6576d95dd05f7ed99a7349ca5d4
nonsense00027 <caneteneilfred@gmail.com>
2020-06-09 07:34:24 +0800

    Header modified

--- dist/js/main.js
+++ dist/js/main.js
@@ -158,3 +158,21 @@ $(".menu-nav a").on("click", function (e) {
     );
   }
 });
+
+$(".logo a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      500,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
